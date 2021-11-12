49d2b7d1b136e0393badf4eeede26b78c91aa189
marziadifilippo <marzia.difilippo@gmail.com>
2021-11-12 16:25:46 +0100

    Modified input parameters

--- pipeline/metabolitesIdentification_FuzzyWuzzy_part2.py
+++ pipeline/metabolitesIdentification_FuzzyWuzzy_part2.py
@@ -14,52 +14,32 @@ OUTDIR = workingDirs[2]
 testModel = sys.argv[1]
 if testModel == 'recon':
     ## Recon 3
-    dfMappingMetaCyc = 'recon3_mappingMetaCyc_allResults'
-    dfMappingKeggC = 'recon3_mappingKeggC_allResults'
-    dfMappingKeggG = 'recon3_mappingKeggG_allResults'
-    dfMappingChebi = 'recon3_mappingChebi_allResults'
-    outputFileName = 'recon3'
+    prefix_modelName = 'recon3D'
 elif testModel == 'y7':
     ## Yeast 7
-    dfMappingMetaCyc = 'yeast7_mappingMetaCyc_allResults'
-    dfMappingKeggC = 'yeast7_mappingKeggC_allResults'
-    dfMappingKeggG = 'yeast7_mappingKeggG_allResults'
-    dfMappingChebi = 'yeast7_mappingChebi_allResults'
-    outputFileName = 'yeast7'
+    prefix_modelName = 'y7'
 elif testModel == 'y8':
     ## Yeast 8
-    dfMappingMetaCyc = 'yeast8_mappingMetaCyc_allResults'
-    dfMappingKeggC = 'yeast8_mappingKeggC_allResults'
-    dfMappingKeggG = 'yeast8_mappingKeggG_allResults'
-    dfMappingChebi = 'yeast8_mappingChebi_allResults'
-    outputFileName = 'yeast8'
+    prefix_modelName = 'y8'
 elif testModel == 'hmr':
     ## HMRcore
-    dfMappingMetaCyc = 'hmrCore_mappingMetaCyc_allResults'
-    dfMappingKeggC = 'hmrCore_mappingKeggC_allResults'
-    dfMappingKeggG = 'hmrCore_mappingKeggG_allResults'
-    dfMappingChebi = 'hmrCore_mappingChebi_allResults'
-    outputFileName = 'hmrCore'
+    prefix_modelName = 'hmrCore'
 elif testModel == 'ownData':
     ## specify your input data
-    dfMappingMetaCyc = ''
-    dfMappingKeggC = ''
-    dfMappingKeggG = ''
-    dfMappingChebi = ''
-    outputFileName = ''
+    prefix_modelName = ''
 
 ### METACYC
-dfMappingMetacyc_all = pd.read_csv(os.path.join(OUTDIR, dfMappingMetaCyc + '.tsv'), sep = '\t', index_col=0)
+dfMappingMetacyc_all = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_allResults.tsv'), sep = '\t', index_col=0)
 dfMatches100, dfMatches91_99, dfMatchesEmpty = metL.filterFWoutputs(dfMappingMetacyc_all)
-dfMatches100.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingMetaCyc_100.tsv'), sep='\t')
-dfMatches91_99.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingMetaCyc_91_99.tsv'), sep='\t')
-dfMatchesEmpty.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingMetaCyc_empty.tsv'), sep='\t')
+dfMatches100.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_100.tsv'), sep='\t')
+dfMatches91_99.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_91_99.tsv'), sep='\t')
+dfMatchesEmpty.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_empty.tsv'), sep='\t')
 
 ## Automatic interaction with the user to curate the list of putative matches with score between 91 and 99
-dfMatches91_99 = pd.read_csv(os.path.join(OUTDIR, outputFileName + '_mappingMetaCyc_91_99.tsv'), sep="\t", index_col = 0)
+dfMatches91_99 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_91_99.tsv'), sep="\t", index_col = 0)
 dfMatches91_99['Matches'] = dfMatches91_99['Matches'].apply(ast.literal_eval)
 
-curatedFWFile = open(os.path.join(OUTDIR, outputFileName + '_mappingMetaCyc_91_99.tsv'), mode='w')
+curatedFWFile = open(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_91_99.tsv'), mode='w')
 gL.writeLineByLineToFile(curatedFWFile, ['Name', 'Matches'], '\t')
 
 for row in dfMatches91_99.itertuples():
@@ -77,18 +57,18 @@ curatedFWFile.close()
 
 
 ## KEGG
-dfMappingKeggC_all = pd.read_csv(os.path.join(OUTDIR, dfMappingKeggC + '.tsv'), sep = '\t', index_col=0)
-dfMappingKeggG_all = pd.read_csv(os.path.join(OUTDIR, dfMappingKeggG + '.tsv'), sep = '\t', index_col=0)
+dfMappingKeggC_all = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_allResults.tsv'), sep = '\t', index_col=0)
+dfMappingKeggG_all = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_allResults.tsv'), sep = '\t', index_col=0)
 dfMatches100, dfMatches91_99, dfMatchesEmpty = metL.filterFWoutputs(dfMappingKeggC_all)
-dfMatches100.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggC_100.tsv'), sep='\t')
-dfMatches91_99.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggC_91_99.tsv'), sep='\t')
-dfMatchesEmpty.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggC_empty.tsv'), sep='\t')
+dfMatches100.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_100.tsv'), sep='\t')
+dfMatches91_99.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_91_99.tsv'), sep='\t')
+dfMatchesEmpty.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_empty.tsv'), sep='\t')
 
 ## Automatic interaction with the user to curate the list of putative matches with score between 91 and 99
-dfMatches91_99 = pd.read_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggC_91_99.tsv'), sep="\t", index_col = 0)
+dfMatches91_99 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_91_99.tsv'), sep="\t", index_col = 0)
 dfMatches91_99['Matches'] = dfMatches91_99['Matches'].apply(ast.literal_eval)
 
-curatedFWFile = open(os.path.join(OUTDIR, outputFileName + '_mappingKeggC_91_99.tsv'), mode='w')
+curatedFWFile = open(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_91_99.tsv'), mode='w')
 gL.writeLineByLineToFile(curatedFWFile, ['Name', 'Matches'], '\t')
 
 for row in dfMatches91_99.itertuples():
@@ -105,15 +85,15 @@ for row in dfMatches91_99.itertuples():
 curatedFWFile.close()
 
 dfMatches100, dfMatches91_99, dfMatchesEmpty = metL.filterFWoutputs(dfMappingKeggG_all)
-dfMatches100.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggG_100.tsv'), sep='\t')
-dfMatches91_99.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggG_91_99.tsv'), sep='\t')
-dfMatchesEmpty.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggG_empty.tsv'), sep='\t')
+dfMatches100.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_100.tsv'), sep='\t')
+dfMatches91_99.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_91_99.tsv'), sep='\t')
+dfMatchesEmpty.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_empty.tsv'), sep='\t')
 
 ## Automatic interaction with the user to curate the list of putative matches with score between 91 and 99
-dfMatches91_99 = pd.read_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggG_91_99.tsv'), sep="\t", index_col = 0)
+dfMatches91_99 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_91_99.tsv'), sep="\t", index_col = 0)
 dfMatches91_99['Matches'] = dfMatches91_99['Matches'].apply(ast.literal_eval)
 
-curatedFWFile = open(os.path.join(OUTDIR, outputFileName + '_mappingKeggG_91_99.tsv'), mode='w')
+curatedFWFile = open(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_91_99.tsv'), mode='w')
 gL.writeLineByLineToFile(curatedFWFile, ['Name', 'Matches'], '\t')
 
 for row in dfMatches91_99.itertuples():
@@ -130,17 +110,17 @@ for row in dfMatches91_99.itertuples():
 curatedFWFile.close()
 
 ## CHEBI
-dfMappingChebi_all = pd.read_csv(os.path.join(OUTDIR, dfMappingChebi + '.tsv'), sep = '\t', index_col=0)
+dfMappingChebi_all = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_allResults.tsv'), sep = '\t', index_col=0)
 dfMatches100, dfMatches91_99, dfMatchesEmpty = metL.filterFWoutputs(dfMappingChebi_all)
-dfMatches100.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingChebi_100.tsv'), sep='\t')
-dfMatches91_99.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingChebi_91_99.tsv'), sep='\t')
-dfMatchesEmpty.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingChebi_empty.tsv'), sep='\t')
+dfMatches100.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_100.tsv'), sep='\t')
+dfMatches91_99.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_91_99.tsv'), sep='\t')
+dfMatchesEmpty.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_empty.tsv'), sep='\t')
 
 ## Automatic interaction with the user to curate the list of putative matches with score between 91 and 99
-dfMatches91_99 = pd.read_csv(os.path.join(OUTDIR, outputFileName + '_mappingChebi_91_99.tsv'), sep="\t", index_col = 0)
+dfMatches91_99 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_91_99.tsv'), sep="\t", index_col = 0)
 dfMatches91_99['Matches'] = dfMatches91_99['Matches'].apply(ast.literal_eval)
 
-curatedFWFile = open(os.path.join(OUTDIR, outputFileName + '_mappingChebi_91_99.tsv'), mode='w')
+curatedFWFile = open(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_91_99.tsv'), mode='w')
 gL.writeLineByLineToFile(curatedFWFile, ['Name', 'Matches'], '\t')
 
 for row in dfMatches91_99.itertuples():
--- pipeline/metabolitesIdentification_joiningData.py
+++ pipeline/metabolitesIdentification_joiningData.py
@@ -14,117 +14,30 @@ OUTDIR = workingDirs[2]
 testModel = sys.argv[1]
 if testModel == 'recon':
     ## Recon 3
-    outputFileName = 'recon3'
-    dfMetsInfer_classicMethod = 'recon3D_metabolites_wInferredIds'
-    dfMetsInfer_fw_metacyc1 = 'recon3_mappingMetaCyc_empty'
-    dfMetsInfer_fw_metacyc2 = 'recon3_mappingMetaCyc_100'
-    dfMetsInfer_fw_metacyc3 = 'recon3_mappingMetaCyc_91_99'
-
-    dfMetsInfer_fw_KC1 = 'recon3_mappingKeggC_empty'
-    dfMetsInfer_fw_KC2 = 'recon3_mappingKeggC_100'
-    dfMetsInfer_fw_KC3 = 'recon3_mappingKeggC_91_99'
-
-    dfMetsInfer_fw_KG1 = 'recon3_mappingKeggG_empty'
-    dfMetsInfer_fw_KG2 = 'recon3_mappingKeggG_100'
-    dfMetsInfer_fw_KG3 = 'recon3_mappingKeggG_91_99'
-
-    dfMetsInfer_fw_C1 = 'recon3_mappingChebi_empty'
-    dfMetsInfer_fw_C2 = 'recon3_mappingChebi_100'
-    dfMetsInfer_fw_C3 = 'recon3_mappingChebi_91_99'
-
+    prefix_modelName = 'recon3D'
 elif testModel == 'y7':
     ## Yeast 7
-    outputFileName = 'yeast7'
-    dfMetsInfer_classicMethod = 'y7_metabolites_wInferredIds'
-
-    dfMetsInfer_fw_metacyc1 = 'yeast7_mappingMetaCyc_empty'
-    dfMetsInfer_fw_metacyc2 = 'yeast7_mappingMetaCyc_100'
-    dfMetsInfer_fw_metacyc3 = 'yeast7_mappingMetaCyc_91_99'
-
-    dfMetsInfer_fw_KC1 = 'yeast7_mappingKeggC_empty'
-    dfMetsInfer_fw_KC2 = 'yeast7_mappingKeggC_100'
-    dfMetsInfer_fw_KC3 = 'yeast7_mappingKeggC_91_99'
-
-    dfMetsInfer_fw_KG1 = 'yeast7_mappingKeggG_empty'
-    dfMetsInfer_fw_KG2 = 'yeast7_mappingKeggG_100'
-    dfMetsInfer_fw_KG3 = 'yeast7_mappingKeggG_91_99'
-
-    dfMetsInfer_fw_C1 = 'yeast7_mappingChebi_empty'
-    dfMetsInfer_fw_C2 = 'yeast7_mappingChebi_100'
-    dfMetsInfer_fw_C3 = 'yeast7_mappingChebi_91_99'
+    prefix_modelName = 'y7'
 elif testModel == 'y8':
     ## Yeast 8
-    outputFileName = 'yeast8'
-    dfMetsInfer_classicMethod = 'y8_metabolites_wInferredIds'
-    dfMetsInfer_fw_metacyc1 = 'yeast8_mappingMetaCyc_empty'
-    dfMetsInfer_fw_metacyc2 = 'yeast8_mappingMetaCyc_100'
-    dfMetsInfer_fw_metacyc3 = 'yeast8_mappingMetaCyc_91_99'
-
-    dfMetsInfer_fw_KC1 = 'yeast8_mappingKeggC_empty'
-    dfMetsInfer_fw_KC2 = 'yeast8_mappingKeggC_100'
-    dfMetsInfer_fw_KC3 = 'yeast8_mappingKeggC_91_99'
-
-    dfMetsInfer_fw_KG1 = 'yeast8_mappingKeggG_empty'
-    dfMetsInfer_fw_KG2 = 'yeast8_mappingKeggG_100'
-    dfMetsInfer_fw_KG3 = 'yeast8_mappingKeggG_91_99'
-
-    dfMetsInfer_fw_C1 = 'yeast8_mappingChebi_empty'
-    dfMetsInfer_fw_C2 = 'yeast8_mappingChebi_100'
-    dfMetsInfer_fw_C3 = 'yeast8_mappingChebi_91_99'
-
+    prefix_modelName = 'y8'
 elif testModel == 'hmr':
     ## HMRcore
-    outputFileName = 'hmrCore'
-    dfMetsInfer_classicMethod = 'hmrCore_metabolites_wInferredIds'
-
-    dfMetsInfer_fw_metacyc1 = 'hmrCore_mappingMetaCyc_empty'
-    dfMetsInfer_fw_metacyc2 = 'hmrCore_mappingMetaCyc_100'
-    dfMetsInfer_fw_metacyc3 = 'hmrCore_mappingMetaCyc_91_99'
-
-    dfMetsInfer_fw_KC1 = 'hmrCore_mappingKeggC_empty'
-    dfMetsInfer_fw_KC2 = 'hmrCore_mappingKeggC_100'
-    dfMetsInfer_fw_KC3 = 'hmrCore_mappingKeggC_91_99'
-
-    dfMetsInfer_fw_KG1 = 'hmrCore_mappingKeggG_empty'
-    dfMetsInfer_fw_KG2 = 'hmrCore_mappingKeggG_100'
-    dfMetsInfer_fw_KG3 = 'hmrCore_mappingKeggG_91_99'
-
-    dfMetsInfer_fw_C1 = 'hmrCore_mappingChebi_empty'
-    dfMetsInfer_fw_C2 = 'hmrCore_mappingChebi_100'
-    dfMetsInfer_fw_C3 = 'hmrCore_mappingChebi_91_99'
-
+    prefix_modelName = 'hmrCore'
 elif testModel == 'ownData':
     ## specify your input data
-    outputFileName = ''
-    dfMetsInfer_classicMethod = ''
-
-    dfMetsInfer_fw_metacyc1 = ''
-    dfMetsInfer_fw_metacyc2 = ''
-    dfMetsInfer_fw_metacyc3 = ''
-
-    dfMetsInfer_fw_KC1 = ''
-    dfMetsInfer_fw_KC2 = ''
-    dfMetsInfer_fw_KC3 = ''
-
-    dfMetsInfer_fw_KG1 = ''
-    dfMetsInfer_fw_KG2 = ''
-    dfMetsInfer_fw_KG3 = ''
-
-    dfMetsInfer_fw_C1 = ''
-    dfMetsInfer_fw_C2 = ''
-    dfMetsInfer_fw_C3 = ''
-
+    prefix_modelName = ''
 
 dMetMapping = {}
 
 # joining outputs from MetaCyc
 dfMetaCyc = pd.read_csv(os.path.join(RAWDIR,'metacyc_compounds_20201216152513.csv'), sep='\t', dtype = str)
 
-df1 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_metacyc1 + '.tsv'), sep='\t', index_col=0)
+df1 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_empty.tsv'), sep='\t', index_col=0)
 df1['Matches'] = df1['Matches'].apply(ast.literal_eval)
-df2 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_metacyc2 + '.tsv'), sep='\t', index_col=0)
+df2 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_100.tsv'), sep='\t', index_col=0)
 df2['Matches'] = df2['Matches'].apply(ast.literal_eval)
-df3 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_metacyc3 + '.tsv'), sep='\t')
+df3 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_91_99.tsv'), sep='\t')
 df3['Matches'] = df3['Matches'].apply(ast.literal_eval)
 
 dfAll = pd.concat([df1,df2,df3])
@@ -150,11 +63,11 @@ dfKeggG['ChebiId'] = dfKeggG['ChebiId'].apply(ast.literal_eval)
 dfKeggC = dfKeggC.explode('Name')
 dfKeggG = dfKeggG.explode('Name')
 
-df1 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_KC1 + '.tsv'), sep='\t', index_col=0)
+df1 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_empty.tsv'), sep='\t', index_col=0)
 df1['Matches'] = df1['Matches'].apply(ast.literal_eval)
-df2 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_KC2 + '.tsv'), sep='\t', index_col=0)
+df2 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_100.tsv'), sep='\t', index_col=0)
 df2['Matches'] = df2['Matches'].apply(ast.literal_eval)
-df3 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_KC3 + '.tsv'), sep='\t')
+df3 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_91_99.tsv'), sep='\t')
 df3['Matches'] = df3['Matches'].apply(ast.literal_eval)
 
 dfAll = pd.concat([df1,df2,df3])
@@ -174,11 +87,11 @@ for row in dfAll.itertuples():
         dMetMapping[row.Name.strip()] += lCorrespondences
 
 
-df1 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_KG1 + '.tsv'), sep='\t', index_col=0)
+df1 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_empty.tsv'), sep='\t', index_col=0)
 df1['Matches'] = df1['Matches'].apply(ast.literal_eval)
-df2 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_KG2 + '.tsv'), sep='\t', index_col=0)
+df2 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_100.tsv'), sep='\t', index_col=0)
 df2['Matches'] = df2['Matches'].apply(ast.literal_eval)
-df3 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_KG3 + '.tsv'), sep='\t')
+df3 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_91_99.tsv'), sep='\t')
 df3['Matches'] = df3['Matches'].apply(ast.literal_eval)
 
 dfAll = pd.concat([df1,df2,df3])
@@ -203,11 +116,11 @@ dfChebiNames = pd.read_csv(os.path.join(RAWDIR, 'chebi_names_20201216.tsv.bz2'),
 dfChebiUniprot = pd.read_csv(os.path.join(RAWDIR, 'chebi_uniprot_20201216.tsv.bz2'), sep = '\t', header=None, dtype=str, compression='bz2', names=['ID','NAME_wSymbols', 'NAME'])
 dfChebiCompounds =  pd.read_csv(os.path.join(RAWDIR, 'chebi_compounds_20201216.tsv.bz2'), sep = '\t', compression='bz2', dtype=str)
 
-df1 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_C1 + '.tsv'), sep='\t', index_col=0)
+df1 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_empty.tsv'), sep='\t', index_col=0)
 df1['Matches'] = df1['Matches'].apply(ast.literal_eval)
-df2 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_C2 + '.tsv'), sep='\t', index_col=0)
+df2 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_100.tsv'), sep='\t', index_col=0)
 df2['Matches'] = df2['Matches'].apply(ast.literal_eval)
-df3 = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_fw_C3 + '.tsv'), sep='\t')
+df3 = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_91_99.tsv'), sep='\t')
 df3['Matches'] = df3['Matches'].apply(ast.literal_eval)
 
 dfAll = pd.concat([df1,df2,df3])
@@ -250,14 +163,14 @@ for met in dMetMapping:
     dMetMapping_woDuplicates[met.strip()] = gL.unique(dMetMapping[met])
 
 dfMatches = pd.DataFrame(dMetMapping_woDuplicates.items(), columns=['Name', 'Identifiers'])
-dfMatches.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingFuzzy.tsv'), sep='\t', index = False)
+dfMatches.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingFuzzy.tsv'), sep='\t', index = False)
 
 
 ## Joining FuzzyWuzzy and output from Step1
-dfFuzzyMatches = pd.read_csv(os.path.join(OUTDIR, outputFileName + '_mappingFuzzy.tsv'), sep='\t')
+dfFuzzyMatches = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingFuzzy.tsv'), sep='\t')
 dfFuzzyMatches['Identifiers_fuzzy'] = dfFuzzyMatches['Identifiers'].apply(ast.literal_eval)
 
-dfClassicMatches = pd.read_csv(os.path.join(OUTDIR, dfMetsInfer_classicMethod + '.csv'), sep='\t')
+dfClassicMatches = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_metabolites_wInferredIds.csv'), sep='\t')
 dfClassicMatches['Identifiers_classic'] = dfClassicMatches['Identifiers'].apply(ast.literal_eval)
 dfClassicMatches['Name'] = dfClassicMatches.Name.str.strip()
 
@@ -265,4 +178,4 @@ dfMerge = pd.merge(dfFuzzyMatches, dfClassicMatches, on = 'Name')
 dfMerge['Identifiers'] = dfMerge.apply(lambda row: gL.unique(row['Identifiers_fuzzy'] + row['Identifiers_classic']), axis=1)
 
 dfMerge_filter = dfMerge[['Name', 'Identifiers_fuzzy', 'Identifiers_classic', 'Identifiers']]
-dfMerge_filter.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingFuzzyAndClassic.tsv'), sep='\t', index = False)
+dfMerge_filter.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingFuzzyAndClassic.tsv'), sep='\t', index = False)
