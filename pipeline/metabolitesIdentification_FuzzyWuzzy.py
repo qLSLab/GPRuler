612cd2a687cac950895b62a5105a40dff485806d
unknown <>
2021-11-12 12:27:40 +0100

    Update pipeline

--- pipeline/metabolitesIdentification_FuzzyWuzzy.py
+++ pipeline/metabolitesIdentification_FuzzyWuzzy.py
@@ -15,32 +15,22 @@ OUTDIR = workingDirs[2]
 testModel = sys.argv[1]
 if testModel == 'recon':
     ## Recon 3
-    modelXml = 'Recon3D_301_20200923'
-    dfmetsInfo = 'recon3D_metabolites'
-    outputFileName = 'recon3'
+    prefix_modelName = 'recon3D'
 elif testModel == 'y7':
     ## Yeast 7
-    modelXml = 'yeast_7.6_cobra'
-    dfmetsInfo = 'y7_metabolites'
-    outputFileName = 'yeast7'
+    prefix_modelName = 'y7'
 elif testModel == 'y8':
     ## Yeast 8
-    modelXml = 'yeast8'
-    dfmetsInfo = 'y8_metabolites'
-    outputFileName = 'yeast8'
+    prefix_modelName = 'y8'
 elif testModel == 'hmr':
     ## HMRcore
-    modelXml = 'HMRcore_20200328_wReconNames'
-    dfmetsInfo = 'hmrCore_metabolites'
-    outputFileName = 'hmrCore'
+    prefix_modelName = 'hmrCore'
 elif testModel == 'ownData':
     ## specify your input data
-    modelXml = ''
-    dfmetsInfo = ''
-    outputFileName = ''
+    prefix_modelName = ''
 
 # extracting metabolites info from the input model
-df = pd.read_csv(os.path.join(OUTDIR, dfmetsInfo + '.csv'), sep = '\t', dtype = str)
+df = pd.read_csv(os.path.join(OUTDIR, prefix_modelName + '_metabolites.csv'), sep = '\t', dtype = str)
 
 dfChebiDb = pd.read_csv(os.path.join(RAWDIR, 'chebi_database_accession_20201216.tsv.bz2'), sep = '\t', compression='bz2', dtype = {'ID': str, 'COMPOUND_ID': str, 'ACCESSION_NUMBER': str})
 dfChebiRelations = pd.read_csv(os.path.join(RAWDIR, 'chebi_relation_20201216.tsv.bz2'), sep = '\t', compression='bz2', dtype = {'ID': str, 'TYPE': str, 'INIT_ID': str, 'FINAL_ID': str})
@@ -60,7 +50,7 @@ lMets2Search = gL.unique(lMets2Search)
 dfMetaCyc = pd.read_csv(os.path.join(RAWDIR,'metacyc_compounds_20201216152513.csv'), sep='\t', usecols=['Id', 'Name'])
 choicesMetaCyc = dfMetaCyc.Name.to_list()
 dfMatches = metL.applyFW2Db(lMets2Search, choicesMetaCyc)
-dfMatches.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingMetaCyc_allResults.tsv'), sep='\t')
+dfMatches.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingMetaCyc_allResults.tsv'), sep='\t')
 
 # query KEGG compounds
 dfKeggC = pd.read_csv(os.path.join(RAWDIR,'kegg_compounds_20201216150802.csv'), sep='\t', usecols=['Id', 'Name'])
@@ -71,11 +61,11 @@ dfKeggC = dfKeggC.explode('Name')
 dfKeggG = dfKeggG.explode('Name')
 choicesKeggC = dfKeggC.Name.to_list() + dfKeggC.Id.to_list()
 dfMatches = metL.applyFW2Db(lMets2Search, choicesKeggC)
-dfMatches.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggC_allResults.tsv'), sep='\t')
+dfMatches.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggC_allResults.tsv'), sep='\t')
 
 choicesKeggG = dfKeggG.Name.to_list() + dfKeggG.Id.to_list()
 dfMatches = metL.applyFW2Db(lMets2Search, choicesKeggG)
-dfMatches.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingKeggG_allResults.tsv'), sep='\t')
+dfMatches.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingKeggG_allResults.tsv'), sep='\t')
 
 # query ChEBI compounds
 dfChebiNames = pd.read_csv(os.path.join(RAWDIR, 'chebi_names_20201216.tsv.bz2'), sep = '\t', compression='bz2', dtype=str)
@@ -84,4 +74,4 @@ dfChebiCompounds =  pd.read_csv(os.path.join(RAWDIR, 'chebi_compounds_20201216.t
 
 choicesChebi = dfChebiNames.NAME.to_list() + dfChebiUniprot.NAME.to_list() + dfChebiCompounds.NAME.to_list()
 dfMatches = metL.applyFW2Db(lMets2Search, choicesChebi)
-dfMatches.to_csv(os.path.join(OUTDIR, outputFileName + '_mappingChebi_allResults.tsv'), sep='\t')
+dfMatches.to_csv(os.path.join(OUTDIR, prefix_modelName + '_mappingChebi_allResults.tsv'), sep='\t')
